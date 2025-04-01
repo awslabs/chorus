9a0ddceb57ca705dc0df86904ef8690a8c9fabe6
Raphael Shu <zhongzhu@amazon.com>
2025-04-01 10:00:57 -0700

    Simplify key developer experience

--- examples/python/basic_single_agent.py
+++ examples/python/basic_single_agent.py
@@ -1,7 +1,6 @@
 from chorus.agents import ConversationalTaskAgent
 from chorus.toolbox import ArxivRetrieverTool
 from chorus.core import Chorus
-from chorus.workspace import NoActivityStopper
 
 if __name__ == '__main__':
     paper_research_agent = ConversationalTaskAgent(
@@ -11,21 +10,19 @@ if __name__ == '__main__':
     )
 
     chorus = Chorus(
-        agents=[paper_research_agent],
-        stop_conditions=[NoActivityStopper()]
+        agents=[paper_research_agent]
     )
 
-    chorus.get_environment().send_message(
-        source="human",
+    chorus.start()
+
+    answer_message = chorus.send_and_wait(
         destination="PaperResearchAgent",
-        content="List papers on Arxiv about speculative decoding."
+        message="List papers on Arxiv about speculative decoding."
     )
-    chorus.run()
+
     # Print out the answer
-    answer_message = chorus.get_environment().filter_messages(
-        source="PaperResearchAgent",
-        destination="human"
-    )[-1]
     print("===========================================")
     print("Final Answer:")
     print(answer_message.content)
+
+    chorus.stop()
